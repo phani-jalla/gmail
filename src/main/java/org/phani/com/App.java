55dabd64720a9e43b71155035fdbe22b06eb1d03
root <root@ip-172-31-92-186.ec2.internal>
2019-04-13 10:28:20 +0000

    commit

---
+++ src/main/java/org/phani/com/App.java
@@ -0,0 +1,13 @@
+package org.phani.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
